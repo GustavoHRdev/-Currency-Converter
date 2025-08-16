2a1fde6fc7c5b29da50f58e0d88a4c11ef85fae6
Gustavo Honorio Ribeiro <gustavohonorio.GR@gmail.com>
2025-08-16 02:24:03 -0300

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/-Currency-Converter/', // ou '/currency-converter/' se renomear o repo
-})
+  base: '/Currency-Converter/', // <--- coloque o nome exato do seu repositório aqui!
+});
